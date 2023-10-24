a97834c2d174f3b5b1bb558a1ecfd7dd6c1ef933
Yawei <yaweihong@icloud.com>
2023-10-24 14:36:05 +0800

    done

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 #!/usr/bin/env node
 
-// 输出命令带的参数
-console.log(process.argv)
+const fs = require('fs')
+const path = require('path')
+
+const filePath = path.resolve(__dirname, process.argv[2])
+const cssContent = fs.readFileSync(filePath, "utf-8")
+
+const lines = cssContent.split("\n")
+let newCss = []
+for (let i = 0; i < lines.length; i++) {
+    const element = lines[i];
+    if (!element || element.indexOf(': "') > 0) {
+        newCss.push(element)
+    } else if (element && !element.startsWith("/*")) {
+        const kv = element.split("/*")[0].split(": ")
+        const key = kv[0].split("-")
+        let newKey = ""
+        for (let j = 0; j < key.length; j++) {
+            const name = key[j];
+            if (j > 0) {
+                newKey += name[0].toUpperCase() + name.slice(1)
+            } else {
+                newKey = name
+            }
+        }
+        const value = kv[1]
+        const p = `${newKey}: "${value}`.replace(";", '",')
+        newCss.push(p)
+    }
+}
+
+const processedCss = newCss.join("\n")
+fs.writeFileSync(filePath, processedCss)
